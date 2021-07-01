874de0dc5b3739d40048c91250232a05e9bd91e4
Adam Sindelar <adam.sindelar@gmail.com>
2021-07-01 23:55:14 +0200

    explicit LayerMatrix constructor

--- physics/src/layer_matrix.h
+++ physics/src/layer_matrix.h
@@ -10,7 +10,8 @@ namespace vstr {
 
 class LayerMatrix {
  public:
-  LayerMatrix(const std::vector<std::pair<uint32_t, uint32_t>>& layer_pairs);
+  explicit LayerMatrix(
+      const std::vector<std::pair<uint32_t, uint32_t>>& layer_pairs);
 
   void Set(uint32_t x, uint32_t y, bool enabled);
   bool Check(uint32_t x, uint32_t y) const;
