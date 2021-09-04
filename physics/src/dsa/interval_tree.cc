822345fd323e115bef6869e6c13053cb524a2e41
Adam Sindelar <adam.sindelar@gmail.com>
2021-09-04 21:18:43 +0200

    Clean up

--- physics/src/dsa/interval_tree.cc
+++ physics/src/dsa/interval_tree.cc
@@ -9,24 +9,6 @@
 
 namespace vstr {
 
-bool operator<(const Interval& x, const Interval& y) {
-  if (x.low == y.low) {
-    return x.high < y.high;
-  }
-  return x.low < y.low;
-}
-
-bool operator>(const Interval& x, const Interval& y) {
-  if (x.low == y.low) {
-    return x.high > y.high;
-  }
-  return x.low > y.low;
-}
-
-bool operator==(const Interval& x, const Interval& y) {
-  return x.low == y.low && x.high == y.high;
-}
-
 std::ostream& operator<<(std::ostream& os, const Interval& interval) {
   return os << "[" << interval.low << ", " << interval.high << ")";
 }
